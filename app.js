a3cfe042660207a48fbff085d4702ab18076e2ef
baoph <baoph@unit.com.vn>
2025-01-20 14:04:26 +0700

    fix some thing css

--- app.js
+++ app.js
@@ -15,136 +15,134 @@ let ang = 0; // Angle in radians
 let spinButtonClicked = false;
 
 const events = {
-    listeners: {},
-    addEventListener: function (eventName, fn) {
-        this.listeners[eventName] = this.listeners[eventName] || [];
-        this.listeners[eventName].push(fn);
-    },
-    fire: function (eventName, ...args) {
-        if (this.listeners[eventName]) {
-            for (let fn of this.listeners[eventName]) {
-                fn(...args);
-            }
-        }
-    },
+	listeners: {},
+	addEventListener: function (eventName, fn) {
+		this.listeners[eventName] = this.listeners[eventName] || [];
+		this.listeners[eventName].push(fn);
+	},
+	fire: function (eventName, ...args) {
+		if (this.listeners[eventName]) {
+			for (let fn of this.listeners[eventName]) {
+				fn(...args);
+			}
+		}
+	},
 };
 
 function random(m, M) {
-    return Math.random() * (M - m) + m;
+	return Math.random() * (M - m) + m;
 }
 
 function getIndex() {
-    return Math.floor(tot - (ang / TAU) * tot) % tot;
+	return Math.floor(tot - (ang / TAU) * tot) % tot;
 }
 
 function drawSector(sector, i) {
-    const ang = arc * i;
-    spinElement.save();
+	const ang = arc * i;
+	spinElement.save();
 
-    // COLOR
-    spinElement.beginPath();
-    spinElement.fillStyle = `hsl(${(360 / sectors.length) * i},100%,75%)`;
-    spinElement.moveTo(rad, rad);
-    spinElement.arc(rad, rad, rad, ang, ang + arc);
-    spinElement.lineTo(rad, rad);
-    spinElement.fill();
+	// COLOR
+	spinElement.beginPath();
+	spinElement.fillStyle = `hsl(${(360 / sectors.length) * i},100%,75%)`;
+	spinElement.moveTo(rad, rad);
+	spinElement.arc(rad, rad, rad, ang, ang + arc);
+	spinElement.lineTo(rad, rad);
+	spinElement.fill();
 
-    // TEXT
-    spinElement.translate(rad, rad);
-    spinElement.rotate(ang + arc / 2);
-    spinElement.textAlign = "right";
-    spinElement.fillStyle = "black";
-    spinElement.font = "30px 'Fira Code', serif";
-    spinElement.fillText(sector, rad - 10, 10);
+	// TEXT
+	spinElement.translate(rad, rad);
+	spinElement.rotate(ang + arc / 2);
+	spinElement.textAlign = "right";
+	spinElement.fillStyle = "black";
+	spinElement.font = "14px 'Fira Code', serif";
+	spinElement.fillText(sector, rad - 10, 10);
 
-    spinElement.restore();
+	spinElement.restore();
 }
 
 function rotate() {
-    const sector = sectors[getIndex()];
-    spinElement.canvas.style.transform = `rotate(${ang - PI / 2}rad)`;
+	const sector = sectors[getIndex()];
+	spinElement.canvas.style.transform = `rotate(${ang - PI / 2}rad)`;
 
-    spinAction.innerText = !angVel ? "Play" : sector;
-    spinAction.style.background = "#6c7ee1";
-    spinAction.style.color = "#fff";
+	spinAction.innerText = !angVel ? "Play" : sector;
+	spinAction.style.background = "#6c7ee1";
+	spinAction.style.color = "#fff";
 }
 
 function frame() {
-    if (!spinButtonClicked) {
-        return;
-    }
+	if (!spinButtonClicked) {
+		return;
+	}
 
-    if (!angVel && spinButtonClicked) {
-        const finalSector = sectors[getIndex()];
-        events.fire("spinEnd", finalSector);
-        spinButtonClicked = false;
-        return;
-    }
+	if (!angVel && spinButtonClicked) {
+		const finalSector = sectors[getIndex()];
+		events.fire("spinEnd", finalSector);
+		spinButtonClicked = false;
+		return;
+	}
 
-    angVel *= friction;
+	angVel *= friction;
 
-    if (angVel < 0.002) angVel = 0;
+	if (angVel < 0.002) angVel = 0;
 
-    ang += angVel;
-    ang %= TAU;
-    rotate();
+	ang += angVel;
+	ang %= TAU;
+	rotate();
 }
 
 function engine() {
-    frame();
-    requestAnimationFrame(engine);
+	frame();
+	requestAnimationFrame(engine);
 }
 
 function init() {
-    sectors.forEach(drawSector);
-    rotate();
-    engine();
-    spinAction.addEventListener("click", () => {
-        if (!sectors.length && !spinButtonClicked) {
-            alert("Please enter your members.");
-            return;
-        }
-        if (!angVel) angVel = random(0.25, 0.45);
-        spinButtonClicked = true;
-    });
+	sectors.forEach(drawSector);
+	rotate();
+	engine();
+	spinAction.addEventListener("click", () => {
+		if (!sectors.length && !spinButtonClicked) {
+			alert("Please enter your members.");
+			return;
+		}
+		if (!angVel) angVel = random(0.25, 0.45);
+		spinButtonClicked = true;
+	});
 }
 
 function setup() {
-    const widthWindow = window.innerWidth;
-    if (widthWindow <= 500) {
-        size = 400;
-    } else if (widthWindow <= 768) {
-        size = 500;
-    } else {
-        size = 600;
-    }
-    spinElement.canvas.width = size;
-    spinElement.canvas.height = size;
-    init();
+	const widthWindow = window.innerWidth;
+	if (widthWindow <= 500) {
+		size = 400;
+	} else if (widthWindow <= 768) {
+		size = 500;
+	} else {
+		size = 600;
+	}
+	spinElement.canvas.width = size;
+	spinElement.canvas.height = size;
+	init();
 }
 
 function handleSubmitForm(e) {
-    e.preventDefault();
-    sectors = [];
-    const textArea = document.querySelector("#form-member textarea");
-    const values = textArea.value;
-    if (!values) {
-        alert("Please enter your data");
-        return;
-    }
-    const result = values
-        .split(",")
-        .map((val) => val.replace(/(\r\n|\n|\r)/gm, ""));
-    sectors.push(...result);
-    tot = sectors.length;
-    arc = TAU / sectors.length;
-    init();
+	e.preventDefault();
+	sectors = [];
+	const textArea = document.querySelector("#form-member textarea");
+	const values = textArea.value;
+	if (!values) {
+		alert("Please enter your data");
+		return;
+	}
+	const result = values.split(",").map((val) => val.replace(/(\r\n|\n|\r)/gm, ""));
+	sectors.push(...result);
+	tot = sectors.length;
+	arc = TAU / sectors.length;
+	init();
 }
 
 formElement.addEventListener("submit", handleSubmitForm);
 window.addEventListener("resize", setup);
 events.addEventListener("spinEnd", (sector) => {
-    alert(`Opp, you won ${sector}`);
+	alert(`Opp, you won ${sector}`);
 });
 
 setup();
